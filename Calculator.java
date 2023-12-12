280a78e884cb9b08861da274633afa26cf5051b0
DEEPSHIKHA TRIPATHI <deepshikha@DEEPSHIKHAs-MacBook-Air.local>
2023-12-12 18:36:02 +0530

    Created Objects

---
+++ Calculator.java
@@ -0,0 +1,83 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Calculator implements ActionListener{
+
+    JFrame frame;
+    JTextField textField;
+    JPanel panel;
+    JButton[] numButtons = new JButton[10];
+    JButton[] funcButtons = new JButton[8];
+    JButton addButton, subButton, mulButton, divButton;
+    JButton decButton, equButton, delButton, clrButton;
+    Font font = new Font("Ink Free", Font.BOLD, 28);
+
+    double num1=0, num2=0, result=0;
+    char operator;
+    Calculator(){
+        frame = new JFrame("Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420,550);
+        frame.setLayout(null);
+
+        textField = new JTextField();
+        textField.setBounds(50,25,300,50);
+        textField.setFont(font);
+        textField.setEditable(false);
+
+        addButton = new JButton("+");
+        subButton = new JButton("-");
+        mulButton = new JButton("*");
+        divButton = new JButton("/");
+        decButton = new JButton(".");
+        equButton = new JButton("=");
+        delButton = new JButton("Delete");
+        clrButton = new JButton("Clear");
+
+        funcButtons[0] = addButton;
+        funcButtons[1] = subButton;
+        funcButtons[2] = mulButton;
+        funcButtons[3] = divButton;
+        funcButtons[4] = decButton;
+        funcButtons[5] = equButton;
+        funcButtons[6] = delButton;
+        funcButtons[7] = clrButton;
+
+       for(int i = 0; i<8; i++){
+           funcButtons[i].addActionListener(this);
+           funcButtons[i].setFont(font);
+           funcButtons[i].setFocusable(true);
+       }
+
+       for(int i = 0; i<10; i++){
+           numButtons[i] = new JButton(String.valueOf(i));
+           numButtons[i].addActionListener(this);
+           numButtons[i].setFont(font);
+           numButtons[i].setFocusable(false);
+       }
+
+       delButton.setBounds(50,430,145,50);
+       clrButton.setBounds(205,430,145,50);
+
+       panel = new JPanel();
+       panel.setBounds(50,100,300,300);
+       panel.setLayout(new GridLayout(4,4,10,10));
+       panel.setBackground(Color.GRAY);
+
+        frame.add(panel);
+        frame.add(delButton);
+        frame.add(clrButton);
+        frame.add(textField);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args){
+        Calculator calc = new Calculator();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
